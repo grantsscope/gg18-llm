0e2a6d61ada7760bd6e74024a5c38c8e961dac54
rohitmalekar <rohitmalekar@gmail.com>
2023-08-23 17:12:27 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -52,7 +52,7 @@ chain = ConversationalRetrievalChain.from_llm(
 		temperature=0.0,
 		model_name='gpt-3.5-turbo'
 		),
-	retriever=vectors.as_retriever(),
+	retriever=vectors.as_retriever(search_type="mmr"),
 	memory=memory,
 	combine_docs_chain_kwargs=chain_type_kwargs,
 	max_tokens_limit=3000
