e0999cfd9ea816ca2f1fd02cfaf046b034bac777
rohitmalekar <rohitmalekar@gmail.com>
2023-08-23 14:44:22 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 import langchain
-from streamlit_chat import message
+#from streamlit_chat import message
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
